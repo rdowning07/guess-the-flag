7877e24e9367748043219e513a10d0b93953151a
Rob Downing <redowning@gmail.com>
2025-10-21 01:39:06 -0700

    Flatten: remove nested folder and inner .git

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Rob Downing on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
